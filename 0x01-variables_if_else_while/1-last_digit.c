0a0e66176a13f6ff03b221ae6d7f665cdb40e91e
Gralav <lavenderwaga@gmail.com>
2022-09-09 09:16:53 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,33 @@
 #include <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
  * main - Entry point
- * @void: Null value
- * Description: Check the last digit of the number
- * Return: Zero value to succesful
+ * Return: Always 0 (Success
  */
 
 int main(void)
 {
 	int n;
+	int L;
+	char str[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n % 10 == 0)
+	L = n % 10;
+
+	if (L > 5)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+		printf("%s %d is %d and is greater than 5\n", str, n, L);
 	}
-	else if (n % 10 < 6)
-	{  
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	}
-	else if (n % 10 > 5)
+	else if (L == 0)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+		printf("%s %d is %d and is 0\n", str, n, L);
+	}
+	else if (L < 6)
+	{
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L)
 	}
 	return (0);
 }
