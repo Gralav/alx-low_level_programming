395e65b773fc3525831b2ce7b57ea78580df540e
Gralav <lavenderwaga@gmail.com>
2022-09-14 22:57:41 -0700

    changes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,14 +8,13 @@
  * 0 otherwise.
  */
 
-int _isupper(int c)
+int main(void)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
