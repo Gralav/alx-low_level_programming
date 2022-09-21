407191a990e00677d4a2dc27494a990419ce5cf4
Gralav <lavenderwaga@gmail.com>
2022-09-21 08:38:02 -0700

    changes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,49 +1,53 @@
-int char_is_seperator(char ch);
+#include "main.h"
 
 /**
- * cap_string - capitalizes first letters of str
- * @str: string to capitalize letters
- * Return: pointer to str
+ * separators - checks and ensure that all string is capitalized
+ * @c: character to be checked
+ * Return: if separator return 1. Otherwise return 0;
  */
 
-char *cap_string(char *str)
+int separator(char c)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	switch (c)
 	{
-		if (i == 0)
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= 32;
-		}
-	if (char_is_seperator(str[i]))
-	{
-		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			str[i + 1] -= 32;
-	}
-	i++;
-}
-return (str);
-}
-/**
- * char_is_seperator - checks if char is a seperator
- * @ch: character to check
- * Return: 1 if seperator, 0 otherwise
- */
-
-int char_is_seperator(char ch)
-{
-	int i = 0;
-	int numSeperators = 13;
-	char seperators[] = { ' ', '\t', '\n', ',',
-		              ';', '.', '!', '?', '"',
-			      '(', ')', '{', '}' };
-
-	while (i < numSeperators)
-	{
-		if (ch == seperators[i++])
+		case ' ':
+		case '\t':
+		case '\n':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
 			return (1);
+		default:
+			return (0);
 	}
-	return (0); /* didn't find one */
+}
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to uppercase
+ * Return: returns the modified string
+ */
+
+char *cap_string(char *s)
+{
+	int count, upper;
+
+	upper = -32;
+	count = 0;
+	while (s[count] != '\0')
+	{
+		if (s[count] >= 'a' && s[count] <= 'z')
+		{
+			if (s[count] == *s || separator(s[count - 1]))
+				s[count] += upper;
+		}
+		count++;
+	}
+	return (s);
 }
