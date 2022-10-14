06a0eef24ccfa60968c87688d6a676acc78f7874
Gralav <lavenderwaga@gmail.com>
2022-10-14 03:39:57 -0700

    changes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,27 +3,22 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - Entry Point
- * @separator: comma space
- * @n: elements to be printed
- * Return: void
+ * print_numbers - Function that prints numbers,
+ *                 followed by a new line.
+ * @separator: This is input separator
+ * @n: This is the input number
  */
-
-
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int i;
+	unsigned int count;
 
-	if (separator == NULL)
-		return;
 	va_start(valist, n);
-
-	for (i = 0; i < n; i++)
+	for (count = 0; count < n; count++)
 	{
 		printf("%d", va_arg(valist, const unsigned int));
-		if (i != (n - 1))
+		if (separator != NULL && count != (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
