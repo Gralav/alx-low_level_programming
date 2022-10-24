d90b205d2833da82896ada9c843560ffdbac71b2
Gralav <lavenderwaga@gmail.com>
2022-10-24 00:43:48 -0700

    changes

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - function that returns the number of elements in a linked list_t
  *            list
- * @h: This is the sigle list
+ * @h: This is the single list
  * Return: The number of elements in a linked list_t list
  */
 size_t list_len(const list_t *h)
