a8b49984533a2eafbacd7d9438ccb7d0a3520731
Gralav <lavenderwaga@gmail.com>
2022-10-31 05:04:33 -0700

    changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,29 @@
 #include "main.h"
 /**
- * binary_to_unit - This funtion convert binary to decimal
- * @b: This is my string of entry
- * Return: This return to a digital number
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int multiplicador = 1, index = 0;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
 	if (b == NULL)
 		return (0);
-	for (index = 0; b[index] != '\0'; index++)
+	while (b[i + 1])
 	{
-		if (b[index] != '0' && b[index] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		i++;
 	}
-	for (index = index - 1; index >= 0; index--)
-	{
-		char currentCharacter;
+	while (i >= 0)
 
-		currentCharacter = b[index];
-		if (currentCharacter == '1')
-		{
-			decimal += multiplicador;
-		}
-		multiplicador = multiplicador * 2;
-		if (index == 0)
-			break;
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
 	}
-	return (decimal);
+	return (res);
 }
