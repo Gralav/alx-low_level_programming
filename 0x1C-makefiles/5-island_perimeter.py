409db925b04a30fc4f4b9fb60ed852c0c50e672c
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-02-23 07:34:31 +0300

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,31 +1,22 @@
- #!/usr/bin/python3
-"""Module that calculates the perimeter of an island in a grid."""
-
-
-def num_water_neighbors(grid, i, j):
-    """Returns the number of water neighbors a cell has in a grid."""
-
-    num = 0
-
-    if i <= 0 or not grid[i - 1][j]:
-        num += 1
-    if j <= 0 or not grid[i][j - 1]:
-        num += 1
-    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-        num += 1
-    if i >= len(grid) - 1 or not grid[i + 1][j]:
-        num += 1
-
-    return num
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island in grid."""
-
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j]:
-                perimeter += num_water_neighbors(grid, i, j)
-
-    return perimeter 
+    """function that returns the perimeter of the island described in grid"""
+    count = 0
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if grid[i][j] == 1:
+                count += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    count -= 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    count -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    count -= 1
+                if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
+                    count -= 1
+    return count
